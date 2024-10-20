3e8dcb0219d396fefcbc1022f6c4f80606eb1a79
Elania-Marvers <renneson.baptiste@gmail.com>
2024-10-20 13:18:08 +0200

    [UPDATE] updating the lib

---
+++ test/test_c_project/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <artic.h>
+
+int main() {
+    printf("Testing artic library...\n");
+    // Call a function from the artic library
+    artic_function();
+    return 0;
+}
---
+++ test/test_cpp_project/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <artic.h>
+
+int main() {
+    std::cout << "Testing artic library..." << std::endl;
+    // Call a function from the artic library
+    artic_function();
+    return 0;
+}
